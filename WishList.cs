9c3bd3d361bf0e13b7bc3564396bab3d285f1816
Crystal Elsey <hello@crystalelsey.com>
2020-05-26 17:09:28 -0500

    initial commit

---
+++ WishList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My wishes are...");
+            List<string> wishes = new List<string>()
+            {
+                "great job", 
+                "European vacation", 
+                "baby hedgehog"
+            };
+            foreach (string wish in wishes)
+            {
+                Console.WriteLine(wish);
+            }
+        }      
+    }
+}
